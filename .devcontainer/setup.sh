98557995ea1c7440f32dd95f1f61187df38f3ac8
friuli-jokyo <26354730+friuli-jokyo@users.noreply.github.com>
2025-06-08 07:39:32 +0000

    init

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,12 @@
+sudo chown vscode .pnpm-store
+sudo apt-get update
+sudo apt-get install -y libwebkit2gtk-4.1-dev \
+  build-essential \
+  curl \
+  wget \
+  file \
+  libxdo-dev \
+  libssl-dev \
+  libayatana-appindicator3-dev \
+  librsvg2-dev
+pnpm i
