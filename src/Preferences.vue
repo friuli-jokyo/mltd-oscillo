0d04a79a35bb6c55956d31b81c8dcdea85aff92f
friuli-jokyo <26354730+friuli-jokyo@users.noreply.github.com>
2025-06-23 00:00:02 +0000

    style preference view

--- src/Preferences.vue
+++ src/Preferences.vue
@@ -38,53 +38,73 @@ function sendForm(): void {
 <template>
     <main class="container">
         <v-container>
-            <label for="eventId">イベント名</label>
-            <select name="eventId" v-model="event">
-                <template v-for="event in mltdEvents">
-                    <option v-if="event.item.name" :key="event.id" :value="event">
-                        {{ event.name }}
-                    </option>
-                </template>
-            </select>
-        </v-container>
-        <v-container>
-            <label for="rankingType">ランキング種別</label>
-            <select name="rankingType" v-model="rankingType">
-                <option v-for="rankingType in rankingTypes" :key="rankingType" :value="rankingType">
-                    {{ rankingType }}
-                </option>
-            </select>
-        </v-container>
-        <v-container>
-            <template v-if="rankingType == 'idolPoint'">
-                <label for="idolId">アイドル</label>
-                <select name="idolId" v-model="idolId">
-                    <template v-for="idol in idols">
-                        <option v-if="idol.id in (idolIds || [])" :key="idol.id" :value="idol.id">
-                            {{ `${idol.id}: ${idol.fullName}` }}
+            <v-row class="justify-center">
+                <v-col align="right" cols="2">
+                    <label for="eventId">イベント名</label>
+                </v-col>
+                <v-col align="left" cols="10">
+                    <select name="eventId" v-model="event">
+                        <template v-for="event in mltdEvents">
+                            <option v-if="event.item.name" :key="event.id" :value="event">
+                                {{ event.name }}
+                            </option>
+                        </template>
+                    </select>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col align="right" cols="2">
+                    <label for="rankingType">ランキング種別</label>
+                </v-col>
+                <v-col align="left" cols="10">
+                    <select name="rankingType" v-model="rankingType">
+                        <option v-for="rankingType in rankingTypes" :key="rankingType" :value="rankingType">
+                            {{ rankingType }}
                         </option>
-                    </template>
-                </select>
-            </template>
+                    </select>
+                </v-col>
+            </v-row>
+            <v-row v-if="rankingType == 'idolPoint'">
+                <v-col align="right" cols="2">
+                    <label for="idolId">アイドル</label>
+                </v-col>
+                <v-col align="left" cols="10">
+                    <select name="idolId" v-model="idolId">
+                        <template v-for="idol in idols">
+                            <option v-if="idol.id in (idolIds || [])" :key="idol.id" :value="idol.id">
+                                {{ `${idol.id}: ${idol.fullName}` }}
+                            </option>
+                        </template>
+                    </select>
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col align="right" cols="2">
+                    <label for="rank">順位</label>
+                </v-col>
+                <v-col align="left" cols="10">
+                    <input type="text" name="rank" v-model="rankRange">
+                </v-col>
+            </v-row>
+            <v-row>
+                <v-col align="center" cols="12">
+                    <v-btn color="primary" @click="sendForm">
+                        OK
+                    </v-btn>
+                </v-col>
+            </v-row>
         </v-container>
-        <v-container>
-            <label for="rank">順位</label>
-            <input type="text" name="rank" v-model="rankRange">
-        </v-container>
-        <v-container>
-            <v-btn color="primary" @click="sendForm">
-                OK
-            </v-btn></v-container>
     </main>
 </template>
 
 <style>
-select {
-    width: 50%;
-    margin: 0.5em 0;
+.container {
+    margin: 0 auto;
 }
 
-label {
-    margin: 0.5em 0;
+select, input {
+    width: 100%;
+    border: 1px solid #ccc;
+    border-radius: 4px;
 }
 </style>
